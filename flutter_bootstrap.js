74572d0f66769b398a10ec3713cb38814c00d2cc
Pawan-bari <pawanbari127@gmail.com>
2026-08-24 16:36:19 +0530

    routing through polyline added

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"425cfb54d01a9472b3e81d9e76fd63a4a44cfb
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1235812503" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "3039297514" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
